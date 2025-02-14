e52a6d7978512d496a79f971117bf922b5ef1321
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-14 10:21:13 +0630

    defining html structure

---
+++ config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        pale_orange: "#ffd9a6",
+        ligth_orange: "#fbb03b",
+        orange: "#f7931e"
+      },
+    },
+  },
+};
